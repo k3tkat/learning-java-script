85c8fceb35ae3b68558d5c619e022dc465489501
Amandeep <amansiwach4444@gmail.com>
2024-07-29 14:04:33 +0530

    on the way of learning java script

---
+++ first.js
@@ -0,0 +1,105 @@
+// console.log("css project")
+// console.log("aman siwach")
+
+
+
+// fullname="tony stark"
+// age=24;
+// price=98.88;
+// x=null;
+// y = undefined; 
+// isfollow = true;
+// isfollow= false;
+// console.log(fullname);
+
+
+// ***example of let keyword
+
+// let age = 23;
+//  age = 24;
+//  age = 26;
+
+
+// ***example of const keyword
+
+//  const age = 22;
+//  age= 24; error aajayega mtlabh hum ise update nahi kar sakte 
+
+// const PI = 2.14;
+// console.log(PI);
+
+
+
+//  blocks example
+
+//  {
+//     let age=24;
+//     age =26;
+    
+//  }
+
+//  {
+//     let age =26;
+//     const age = 22; error aajayega agar do keyword ek hi block mein use karenge to
+//  }
+
+
+
+// *** number data tye example
+
+//  let age = 24;
+
+
+
+//*** string ex 
+
+// let fullname = "batman";
+
+
+
+// *** boolean ex
+
+// isFollow = true
+
+
+
+
+//  **** example of undefined
+
+// let x;
+
+
+
+// **** null ex
+
+// let x = null;
+
+
+// making collection of a student
+
+// const student = {
+//     fullName:20,
+//     age:28,
+//     copa:80.2,
+//     isPass: true,
+// };
+
+// student["age"] = student["age"] +  1;
+// console.log("age");
+
+
+
+// Chapter 2   ************************************************ 
+
+//  arithmetics operators
+
+ let a = 5;
+ let b = 2;
+
+ console.log("a + b =", a + b);
+ console.log("a - b =", a - b);
+ console.log("a * b =", a * b);
+ console.log("a / b =", a / b);
+ 
+
+
