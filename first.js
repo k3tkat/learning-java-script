f2dc916143ec91f018580e1df0d3094302f49429
Amandeep <amansiwach4444@gmail.com>
2024-08-03 13:59:13 +0530

    Update first.js

--- first.js
+++ first.js
@@ -93,13 +93,148 @@
 
 //  arithmetics operators
 
- let a = 5;
- let b = 2;
+// let a = 5;
+ //let b = 2;
 
- console.log("a + b =", a + b);
- console.log("a - b =", a - b);
- console.log("a * b =", a * b);
- console.log("a / b =", a / b);
+ //console.log("a + b =", a + b);
+ //console.log("a - b =", a - b);
+ //console.log("a * b =", a * b);
+ //console.log("a / b =", a / b);
+//  console.log("a % b =", a % b);
+//  console.log("a**b =", a ** b);
+
+//  console.log("a++ =", a++ );
+//  console.log("++a =", ++a );
+//  console.log("a-- =", a-- );
+//  console.log("--a =", --a );
+ 
+
+// Assignment opperatores***************
+
+// let age = 5;
+// let b = 2;
+
+// a += 4;
+// console.log("a = ", a);
+
+// comparision opperatores***************
+
+//  let a = 5;
+//  let b = 2;
+
+//  console.log("5 == 2", a == b);
+
+//  let a = 5;
+//  let b = 5;
+
+//  console.log("5 == 5", a == b);
+
+
+
+// let a = 5;
+// let b = 2;
+
+// console.log("5 != 2", a != b);
+
+// let a = 5;
+// let b = 2;
+
+// console.log("5 === 2", a === b);
+
+// let a = 5;
+// let b = 2;
+
+// console.log("5 !== 2", a !== b);
+
+
+//logical operators *********************
+
+// let a = 6;
+// let b = 5;
+
+// let cond1 = a
+
+
+//primitive value
+//Reference value
+
+// var a = 12;   a ki value 12 hai 
+// var b = a;    b ki value same karne ke liye hum ye us karenge 
+
+// b = b+2;      aur fie b mein 2 add karne ke liye hum ye karenege 
+
+// var a = [ 12,13];
+// var b = a;
+
+// b.pop();
+
+// ***********conditionals =  1/ if   2/ else   3/ else-if 
+
+
+
+
+
+//******so what is conditionals  
+
+// jab bhee bat agar magar par aayegi to ya fir baa taayegi aaisa hua to ye karo ya wasa hua to wo karo 
+
+//if (10>9)  matlabh 10 9 se bada hai   true
+// if (10<9) matlabh 9 10 se bada hai   false
+
+// if (11<10) { so basically agar if false hai to else kam karega agar if true hai to else kam nahhi karega 
+// }
+// else {
+ //   console.log()
+//  }
+
+
+
+//******loop  -- loops are two type  for or while
+
+// for(var i = 0; i<51; i++){
+//     console.log(i);
+// }
+
+// while loop  ending haiiiii
+
+
+
+// functions -************************
+// functions mainly 3 kaam ke liye hote hain 
+//1 jub aap apna code turant nahi chalana chahte future mein chalana chahte ho to use hota hai
+//2 jub aap apna code resume karna chahte ho 
+//3 jub aap apna code chalana chahte ho but har bar with diffrent data
+  
+
+// function abcd(a,b,c) {
+//     console.log (a, b, c);
+// }
+
+// abcd(12, 13, 14);
+
+// function abcd(n,e,r,u){
+//     console.log(n,e,r,u);
+
+// }
+// abcd(15,16,17,18)
+
+// arrays *************=
+// hum ek varible mein ek value store kar pate hain par jub humyy ek se jyada value store
+// karni ho to fir use hota hai array ka, matlabh ki arry apko freedom deta hai ek se jyaada value use kaarne ki.
+
+//array = group of values.
+
+// var arr = [11,12,13,15,15];
+
+// arr[0,1,2,3,4];
+
+// push pop shift unshift
+
+// var arr = [1,2,3,4,5,6];
+
+//arr.unshift(1);
+//arr.shift(1);
+// arr.splice(1,4);
  
 
 
